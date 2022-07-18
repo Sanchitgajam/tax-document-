f341b01c4c4b8c3c3f0cf4651c2c5fed61e2fcdf
Sanchitgajam <48102172+Sanchitgajam@users.noreply.github.com>
2022-07-18 17:27:35 +0530

    Revert "code"
    This reverts commit 27fcca16634ad715da6fb61fb9dda49b5a7deba3.

--- resources/views/teamwork/emails/invite.blade.php
+++
@@ -1,2 +0,0 @@
-You have been invited to join team {{ $team->name }}.<br>
-Click here to join: <a href="{{ route('register', ['invitation_token' => $invite->accept_token]) }}">{{ route('register', ['invitation_token' => $invite->accept_token]) }}</a>
