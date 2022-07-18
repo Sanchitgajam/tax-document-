5734490e8b314dd4d1bfd1380e94df57e424deef
Sanchitgajam <48102172+Sanchitgajam@users.noreply.github.com>
2022-07-18 17:28:25 +0530

    d

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,127 @@
+<?php return array (
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facades\\Debugbar',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'laracasts/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laracasts\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'laravel/breeze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Breeze\\BreezeServiceProvider',
+    ),
+  ),
+  'laravel/nexmo-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\VonageChannelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Vonage' => 'Illuminate\\Notifications\\Facades\\Vonage',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laraveldaily/larastarters' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'LaravelDaily\\Larastarters\\LarastartersServiceProvider',
+    ),
+  ),
+  'matthewbdaly/laravel-azure-storage' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Matthewbdaly\\LaravelAzureStorage\\AzureStorageServiceProvider',
+    ),
+  ),
+  'mpociot/teamwork' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mpociot\\Teamwork\\TeamworkServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Teamwork' => 'Mpociot\\Teamwork\\Facades\\Teamwork',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nexmo/laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Nexmo\\Laravel\\NexmoServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Nexmo' => 'Nexmo\\Laravel\\Facade\\Nexmo',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
